6e3862f08107ebddbbc5b54d30a30c122fc161a4
Henry Lloyd-Laney <henney@clpc451.cs.ox.ac.uk>
2019-10-18 14:20:24 +0100

    experimenting with the effect of kinetic and resitance parameters at different frequencies

---
+++ FTacv_experiments/Inferred_params/MCMC_plots.py
@@ -0,0 +1,156 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pints
+import pints.plot
+import os
+import matplotlib.ticker as ticker
+def chain_appender(chains, param):
+    new_chain=chains[0, 5000:, param]
+    for i in range(1, len(chains)):
+        new_chain=np.append(new_chain, chains[i, 5000:, param])
+    return new_chain
+def plot_params(titles, set_chain):
+    for i in range(0, len(titles)):
+        print i
+        axes=plt.subplot(3,5,i+1)
+        plot_chain=chain_appender(set_chain, i)
+        if abs(np.mean(plot_chain))<0.001:
+            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3e'))
+        else:
+            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3f'))
+        axes.hist(plot_chain, alpha=0.4,bins=20, stacked=True, edgecolor='black')
+        lb, ub = axes.get_xlim( )
+        axes.set_xticks(np.linspace(lb, ub, 4))
+        axes.set_xlabel(titles[i])
+        axes.set_ylabel('frequency')
+        axes.set_title(graph_titles[i])
+    #if titles[i]!='$\sigma$':
+    #    axes.axvline(true_params[i],color="black", linestyle="--")
+    #axes.legend()
+            #axes.set_xlim((0.9*true_params[i], 1.1*true_params[i]))
+    #axes=plt.subplot(3,4,i+1)
+    #axes.axis('off')
+
+files=os.listdir('.')
+unit_dict={
+    "E_0": "V",
+    'E_start': "V", #(starting dc voltage - V)
+    'E_reverse': "V",
+    'omega':"Hz",#8.88480830076,  #    (frequency Hz)
+    'd_E': "V",   #(ac voltage amplitude - V) freq_range[j],#
+    'v': '$s^{-1}$',   #       (scan rate s^-1)
+    'area': '$cm^{2}$', #(electrode surface area cm^2)
+    'Ru': "$\\Omega$",  #     (uncompensated resistance ohms)
+    'Cdl': "F", #(capacitance parameters)
+    'CdlE1': "",#0.000653657774506,
+    'CdlE2': "",#0.000245772700637,
+    'CdlE3': "",#1.10053945995e-06,
+    'gamma': 'mol cm^{-2}$',
+    'k_0': 's^{-1}$', #(reaction rate s-1)
+    'alpha': "",
+    "E0_mean":"V",
+    "E0_std": "V",
+    "k0_shape":"",
+    "k0_loc":"",
+    "k0_scale":"",
+    "cap_phase":"rads",
+    'phase' : "rads",
+    "":"",
+    "noise":"",
+}
+fancy_names={
+    "E_0": '$E^0$',
+    'E_start': '$E_{start}$', #(starting dc voltage - V)
+    'E_reverse': '$E_{reverse}$',
+    'omega':'$\\omega$',#8.88480830076,  #    (frequency Hz)
+    'd_E': "$\\Delta E$",   #(ac voltage amplitude - V) freq_range[j],#
+    'v': "v",   #       (scan rate s^-1)
+    'area': "Area", #(electrode surface area cm^2)
+    'Ru': "Ru",  #     (uncompensated resistance ohms)
+    'Cdl': "$C_{dl}$", #(capacitance parameters)
+    'CdlE1': "$C_{dlE1}$",#0.000653657774506,
+    'CdlE2': "$C_{dlE2}$",#0.000245772700637,
+    'CdlE3': "$C_{dlE3}$",#1.10053945995e-06,
+    'gamma': '$\\Gamma',
+    'k_0': '$k_0', #(reaction rate s-1)
+    'alpha': "$\\alpha$",
+    "E0_mean":"$E^0 \\mu$",
+    "E0_std": "$E^0 \\sigma$",
+    "cap_phase":"Capacitance phase",
+    'phase' : "Phase",
+    "":"Experiment",
+    "noise":"$\sigma$",
+}
+Titles={
+    'omega':'Input frequency',#8.88480830076,  #    (frequency Hz)
+    'd_E': "Amplitude",   #(ac voltage amplitude - V) freq_range[j],#
+    'v': "Scan rate",   #       (scan rate s^-1)
+    'area': "Area", #(electrode surface area cm^2)
+    'Ru': "Uncompensated resistance",  #     (uncompensated resistance ohms)
+    'Cdl': "Linear capacitance", #(capacitance parameters)
+    'CdlE1': "First order capacitance",#0.000653657774506,
+    'CdlE2': "Second order capacitance",#0.000245772700637,
+    'CdlE3': "Third order capacitance",#1.10053945995e-06,
+    'gamma': 'Surface coverage',
+    'k_0': 'Rate constant', #(reaction rate s-1)
+    'alpha': "Symmetry factor",
+    "E0_mean":"Themodynamic mean",
+    "E0_std": "Thermodynamic standard deviation",
+    "cap_phase":"Capacitance phase",
+    'phase' : "Phase",
+    "":"Experiment",
+    "noise":"Noise",
+}
+#f=open(filename, "r")
+optim_list=['E0_mean',"E0_std",'k_0', 'Ru',"Cdl","CdlE1", "CdlE2",'gamma',"phase", "cap_phase", "alpha", "noise"]
+titles=[fancy_names[x]+"("+unit_dict[x]+")" if (unit_dict[x]!="") else fancy_names[x] for x in optim_list]
+graph_titles=[Titles[x] for x in optim_list]
+true_params=[-0.4, 1e1, 0.000134*1e4,20.0]
+noise_vals=[0.01, 0.02, 0.005, 0]
+counter=-1
+extension=".txt"
+chain_select=1
+j=0
+#fig=plt.figure(num=None, figsize=(12,9), dpi=120, facecolor='w', edgecolor='k')
+filename_list=[]
+number_list=[]
+num_params=len(optim_list)
+electrode="Yellow"
+folder="MCMC_runs"
+for i in range(1, 6):
+    filename="Noramp_3_fourier_MCMC_"+str(i)
+    chains=np.load(("/").join([electrode, folder, filename]))
+#chains2=np.load('GC4_MCMC_1_low_ru')
+    pints.plot.trace(chains)
+    #plot_params(titles,chains)
+    #plt.subplots_adjust(left=0.08, bottom=0.09, right=0.95, top=0.92, wspace=0.30, hspace=0.33)
+#plot_params(titles, chains2)
+    plt.show()
+"""
+optim_list=['E_0','k_0', 'Ru',"Cdl","CdlE1", "CdlE2",'gamma', "omega","phase", "cap_phase", "alpha","noise"]
+titles=[fancy_names[x]+"("+unit_dict[x]+")" if (unit_dict[x]!="") else fancy_names[x] for x in optim_list]
+plt.show()
+for i in range(0, len(titles)):
+    print titles[i]
+
+    axes=plt.subplot(3,5,i+1)
+    if optim_list[i]=="alpha":
+        plt.axvline(0.5, color="black", linestyle="--")
+        axes.set_xlabel(titles[i])
+        axes.set_ylabel('frequency')
+        axes.set_title(graph_titles[i])
+        continue
+    plot_chain=chain_appender(chains2, j)
+    if abs(np.mean(plot_chain))<0.001:
+        axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3e'))
+    else:
+        axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3f'))
+    axes.hist(plot_chain, alpha=0.4,bins=20, stacked=True, edgecolor='black')
+    lb, ub = axes.get_xlim( )
+    axes.set_xticks(np.linspace(lb, ub, 4))
+    axes.set_xlabel(titles[i])
+    axes.set_ylabel('frequency')
+    axes.set_title(graph_titles[i])
+    j+=1
+plt.show()
+"""
--- FTacv_experiments/plotting_simulations_kinetic_params.py
+++ FTacv_experiments/plotting_simulations_kinetic_params.py
@@ -9,14 +9,14 @@ from harmonics_plotter import harmonics
 print "import", time.time()-start
 types=["current", "voltage"]
 start=time.time()
-noramp_startup=FTACV_initialisation(experimental_fitting=True, file_dict={"GC4_1_cv":types, "GC4_2_cv":types, "GC4_3_cv":types}, dec_amount=4)
-ramp_startup=FTACV_initialisation(experimental_fitting=True, file_dict={"GC4_1_ramp_cv":types,}, dec_amount=64)
-print ramp_startup.current_results
+noramp_startup=FTACV_initialisation(experimental_fitting=False, file_dict={}, dec_amount=4)
+ramp_startup=FTACV_initialisation(experimental_fitting=False, file_dict={}, dec_amount=64)
 print "read", time.time()-start
+val=500
 simulation_options={
-    "no_transient":False,
+    "no_transient":val,
     "numerical_debugging": False,
-    "experimental_fitting":True,
+    "experimental_fitting":False,
     "dispersion":False,
     "dispersion_bins":40,
     "test": False,
@@ -29,7 +29,7 @@ simulation_options={
 ramped_simulation_options={
     "no_transient":False,
     "numerical_debugging": False,
-    "experimental_fitting":True,
+    "experimental_fitting":False,
     "dispersion":False,
     "dispersion_bins":40,
     "test": False,
@@ -42,9 +42,9 @@ ramped_simulation_options={
 noramp_other_values={
     "filter_val": 0.5,
     "harmonic_range":range(3,7,1),
-    "experiment_time": noramp_startup.time_results["GC4_1_cv"],
-    "experiment_current":noramp_startup.current_results["GC4_1_cv"],
-    "experiment_voltage":noramp_startup.voltage_results["GC4_1_cv"],
+    "experiment_time": None,#noramp_startup.time_results["GC4_1_cv"],
+    "experiment_current":None,#noramp_startup.current_results["GC4_1_cv"],
+    "experiment_voltage":None,#noramp_startup.voltage_results["GC4_1_cv"],
     "bounds_val":20,
     "signal_length":int(2e4),
 }
@@ -52,23 +52,45 @@ noramp_other_values={
 ramped_other_values={
     "filter_val": 0.5,
     "harmonic_range":range(1,9,1),
-    "experiment_time": ramp_startup.time_results["GC4_1_ramp_cv"],
-    "experiment_current": ramp_startup.current_results["GC4_1_ramp_cv"],
-    "experiment_voltage":ramp_startup.voltage_results["GC4_1_ramp_cv"],
+    "experiment_time": None,#ramp_startup.time_results["GC4_1_ramp_cv"],
+    "experiment_current": None,#ramp_startup.current_results["GC4_1_ramp_cv"],
+    "experiment_voltage":None,#ramp_startup.voltage_results["GC4_1_ramp_cv"],
     "bounds_val":20,
     "signal_length":int(5e5),
 }
+param_bounds={
+    'E_0':[0.2, 0.3],#[param_list['E_start'],param_list['E_reverse']],
+    'omega':[0.95*noramp_startup.generic_noramp_params['omega'],1.05*noramp_startup.generic_noramp_params['omega']],#8.88480830076,  #    (frequency Hz)
+    'Ru': [40, 700],  #     (uncompensated resistance ohms)
+    'Cdl': [0,1e-4], #(capacitance parameters)
+    'CdlE1': [-0.05,0.15],#0.000653657774506,
+    'CdlE2': [-0.01,0.01],#0.000245772700637,
+    'CdlE3': [-0.01,0.01],#1.10053945995e-06,
+    'gamma': [1e-11,1e-9],
+    'k_0': [10, 1e3], #(reaction rate s-1)
+    'alpha': [0.4, 0.6],
+    "cap_phase":[0, 2*math.pi],
+    "E0_mean":[0.19, 0.208],
+    "E0_std": [0.001, 0.2],
+    "k0_shape":[0,2],
+    "k0_loc":[0, 1e3],
+    "k0_scale":[0,1e3],
+    "k0_range":[1e2, 1e4],
+    'phase' : [0, 2*math.pi]
+}
 plt.rcParams.update({'font.size': 14})
-noramp_simulations=single_electron(noramp_startup.generic_noramp_params, simulation_options, noramp_other_values)
-ramped_simulations=single_electron(ramp_startup.generic_ramped_params, ramped_simulation_options, ramped_other_values)
+noramp_simulations=single_electron(None, noramp_startup.generic_noramp_params, simulation_options, noramp_other_values, param_bounds)
+#noramp_simulations=single_electron(ramp_startup.generic_ramped_params, ramped_simulation_options, ramped_other_values)
 harm_class=harmonics(noramp_other_values["harmonic_range"],noramp_simulations.nd_param.omega, 0.2)
-ramped_simulations.def_optim_list(["k_0", "omega"])
+noramp_simulations.def_optim_list(["k_0", "omega"])
 omegas=[math.pi*2**x for x in range(1, 6)]
-k_s=np.flip([10**x for x in range(1, 5)])
+k_s=np.flip([5**x for x in range(1, 8)])
+#k_s=np.linspace(0.1, 0.3, 5)
 fig, ax =plt.subplots(1, len(omegas))
 for i in range(0, len(omegas)):
     for j in range(0, len(k_s)):
-        ax[i].plot(ramped_simulations.time_vec, ramped_simulations.i_nondim(1e3*ramped_simulations.test_vals([k_s[j], omegas[i]], "timeseries")), label="$k^0=$"+str(k_s[j]))
+        voltages=noramp_simulations.define_voltages()
+        ax[i].plot(voltages[val:], noramp_simulations.i_nondim(1e3*noramp_simulations.test_vals([k_s[j], 8.94], "timeseries")), label="$k^0=$"+str(k_s[j]))
     ax[i].set_title('$\\omega$='+ str(omegas[i]/math.pi)+"$\pi$")
     ax[i].set_xlabel("Time(s)")
     ax[i].set_ylabel("Current(mA)")
