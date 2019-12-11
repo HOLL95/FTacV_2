ssh push test - also fixed synthetic time calculation

FTacv_experiments/Inferred_params/Red_MCMC_plots.py
@@ -63,6 +63,28 @@ def plot_params(titles, set_chain, positions=None, label=None):
         axes.set_xlabel(titles[i])
         axes.set_ylabel('frequency')
         axes.set_title(graph_titles[i])
+def trace_plots(titles, chains, names, rhat=False):
+    row, col=det_subplots(len(titles))
+    rhat_vals=pints.rhat_all_params(chains[:, 15000:, :])
+    for i in range(0, len(titles)):
+        axes=plt.subplot(row,col,i+1)
+
+        for j in range(0, len(chains)):
+            axes.plot(chains[j, :, i], label="Chain "+str(j), alpha=0.7)
+        if abs(np.mean(chains[j, :, i]))<0.001:
+            axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2e'))
+        else:
+            axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.3f'))
+        if i==0:
+            axes.legend()
+        #lb, ub = axes.get_xlim( )
+        #axes.set_xticks(np.linspace(lb, ub, 5))
+        if rhat == True:
+            axes.set_title(names[i]+ " (Rhat="+str(round(rhat_vals[i],3))+")")
+        else:
+            axes.set_title(names[i])
+        axes.set_ylabel(titles[i])
+        axes.set_xlabel('Iteration')
 unit_dict={
     "E_0": "V",
     'E_start': "V", #(starting dc voltage - V)
@@ -155,16 +177,19 @@ file_numbers=["8_94","114", "209"]
 positions=[params.index(x) for x in optim_list]
 
 for filename in files:#
-    if ("MCMC_2" in filename) and ("1e-1M" in filename): #and (extension in filename) and (desired_file in filename):
+    if ("MCMC_2" in filename) and ("1e0M" in filename): #and (extension in filename) and (desired_file in filename):
         print(filename)
         cv_idx=filename.index("cv")
         number=filename[cv_idx-2]
 
         chains=np.load(("/").join([electrode, folder, filename]))
+        trace_plots(titles, chains, graph_titles, rhat=True)
+        plt.show()
             #print(number, pints.rhat_all_params(chains[:, 40000:, :]), np.mean(pints.rhat_all_params(chains)))
-        plot_params(titles, chains[:, 25000:, :], positions=positions, label="0.1 M NaCl-Scan "+number)
+        #plot_params(titles, chains[:, 25000:, :], positions=positions, label="0.1 M NaCl-Scan "+number)
         #pints.plot.trace(chains)
         #plt.show()
+    """
     elif ("MCMC_3" in filename) and ("1e0M" in filename): #and (extension in filename) and (desired_file in filename):
         print(filename)
         cv_idx=filename.index("cv")
@@ -175,6 +200,7 @@ for filename in files:#
         plot_params(titles, chains[:, 25000:, :], positions=positions, label="1 M NaCl-Scan "+number)
         #pints.plot.trace(chains)
         #plt.show()
+    """
 #chains2=np.load('GC4_MCMC_1_low_ru')
 #chains2=np.load('GC4_MCMC_1_low_ru')
 

FTacv_experiments/Inferred_params/Reducing_MCMC_plots.py
@@ -53,16 +53,40 @@ def plot_params(titles, set_chain, positions=None, label=None):
         axes=plt.subplot(row,col,i+1)
         plot_chain=chain_appender(set_chain, positions[i])
         if abs(np.mean(plot_chain))<0.001:
-            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3e'))
+            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2e'))
         else:
-            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3f'))
         axes.hist(plot_chain, alpha=0.4,bins=20, stacked=True, label=label)
         axes.legend()
         lb, ub = axes.get_xlim( )
-        axes.set_xticks(np.linspace(lb, ub, 5))
+        axes.set_xticks(np.linspace(lb, ub, 4))
         axes.set_xlabel(titles[i])
         axes.set_ylabel('frequency')
         axes.set_title(graph_titles[i])
+def trace_plots(titles, chains, names, rhat=False):
+    row, col=det_subplots(len(titles))
+    rhat_vals=pints.rhat_all_params(chains[:, 15000:, :])
+    for i in range(0, len(titles)):
+        axes=plt.subplot(row,col,i+1)
+
+        for j in range(0, len(chains)):
+            axes.plot(chains[j, :, i], label="Chain "+str(j), alpha=0.7)
+        if abs(np.mean(chains[j, :, i]))<0.001:
+            axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.3e'))
+        else:
+            axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+        if i==0:
+            axes.legend()
+        #lb, ub = axes.get_xlim( )
+        #axes.set_xticks(np.linspace(lb, ub, 5))
+        if rhat == True:
+            axes.set_title(names[i]+ " (Rhat="+str(round(rhat_vals[i],3))+")")
+        else:
+            axes.set_title(names[i])
+        axes.set_ylabel(titles[i])
+        axes.set_xlabel('Iteration')
+
+
 unit_dict={
     "E_0": "V",
     'E_start': "V", #(starting dc voltage - V)
@@ -153,22 +177,24 @@ desired_file="Noramp_"
 file_numbers=["8_94","114", "209"]
 #concs=["__{0}__".format(x) for x in file_numbers]
 positions=[params.index(x) for x in optim_list]
+oscs=["25", "15", "10"]
 
 for filename in files:#
-    if ("25" in filename):# and ("1e-1M" in filename): #and (extension in filename) and (desired_file in filename):
+    if True in [x in filename for x in oscs]:# and ("1e-1M" in filename): #and (extension in filename) and (desired_file in filename):
         print(filename)
         cv_idx=filename.index("cv")
         number=filename[cv_idx-2]
 
         chains=np.load(("/").join([electrode, folder, filename]))
         print(number, pints.rhat_all_params(chains[:, 20000:, :]), np.mean(pints.rhat_all_params(chains)))
-        plot_params(titles, chains[:, 25000:, :], positions=positions, label="0.1 M NaCl-Scan "+number)
-        #pints.plot.trace(chains)
-        #plt.show()
+        plot_params(titles, chains[:, 25000:, :], positions=positions, label=filename[-2:])
+        #trace_plots(titles, chains, graph_titles, rhat=True)
+
+plt.show()
 #chains2=np.load('GC4_MCMC_1_low_ru')
 #chains2=np.load('GC4_MCMC_1_low_ru')
 
-plt.show()
+#plt.show()
     #
     #plt.subplots_adjust(left=0.08, bottom=0.09, right=0.95, top=0.92, wspace=0.30, hspace=0.33)
 #plot_params(titles, chains2)

FTacv_experiments/Inferred_params/Yellow_MCMC_plots.py
@@ -55,14 +55,36 @@ def plot_params(titles, set_chain, positions=None, label=None):
         if abs(np.mean(plot_chain))<0.001:
             axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3e'))
         else:
-            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.3f'))
+            axes.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
         axes.hist(plot_chain, alpha=0.4,bins=20, stacked=True, label=label)
         axes.legend()
         lb, ub = axes.get_xlim( )
-        axes.set_xticks(np.linspace(lb, ub, 4))
+        axes.set_xticks(np.linspace(lb, ub, 5))
         axes.set_xlabel(titles[i])
         axes.set_ylabel('frequency')
         axes.set_title(graph_titles[i])
+def trace_plots(titles, chains, names, rhat=False):
+    row, col=det_subplots(len(titles))
+    rhat_vals=pints.rhat_all_params(chains[:, 15000:, :])
+    for i in range(0, len(titles)):
+        axes=plt.subplot(row,col,i+1)
+
+        for j in range(0, len(chains)):
+            axes.plot(chains[j, :, i], label="Chain "+str(j), alpha=0.7)
+        if abs(np.mean(chains[j, :, i]))<0.001:
+            axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2e'))
+        else:
+            axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.3f'))
+        if i==0:
+            axes.legend()
+        #lb, ub = axes.get_xlim( )
+        #axes.set_xticks(np.linspace(lb, ub, 5))
+        if rhat == True:
+            axes.set_title(names[i]+ " (Rhat="+str(round(rhat_vals[i],3))+")")
+        else:
+            axes.set_title(names[i])
+        axes.set_ylabel(titles[i])
+        axes.set_xlabel('Iteration')
 unit_dict={
     "E_0": "V",
     'E_start': "V", #(starting dc voltage - V)
@@ -133,14 +155,15 @@ Titles={
     "noise":"Noise",
 }
 #f=open(filename, "r")
-params=(["E0_mean", "E0_std","k_0","Ru","Cdl","CdlE1", "CdlE2","omega","gamma","cap_phase","phase","alpha"])
+params=(["E0_mean", "E0_std","k_0","Ru","Cdl","CdlE1", "CdlE2","omega","gamma","cap_phase","phase","alpha", "noise"])
 optim_list=params
 titles=[fancy_names[x]+"("+unit_dict[x]+")" if (unit_dict[x]!="") else fancy_names[x] for x in optim_list]
 graph_titles=[Titles[x] for x in optim_list]
-folder="MCMC_runs/omega_nondim"
+folder="MCMC_runs/Steady_state"
 electrode="Yellow"
 path=("/").join([dir_path , electrode, folder])
 files=os.listdir(path)#
+print(files)
 #fig=plt.figure(num=None, figsize=(12,9), dpi=120, facecolor='w', edgecolor='k')
 concs1=["1e-1", "5e-1", "1e0", "15e-1", "3e0"]
 concs=["__{0}__".format(x) for x in concs1]
@@ -153,18 +176,17 @@ desired_file="Noramp_"
 file_numbers=["8_94","114", "209"]
 #concs=["__{0}__".format(x) for x in file_numbers]
 positions=[params.index(x) for x in optim_list]
-for i in range(0, len(concs)):
-    for filename in files:#
-        print(filename, desired_file+concs[i]+extension)
-        if filename==(desired_file+concs[i]+extension):#(concs[i] in filename) and (extension in filename) and (desired_file in filename):
-            print(filename, concs[i])
+#for i in range(0, len(concs)):
+for filename in files:#
+    
+    #if filename==(desired_file+concs[i]+extension):#(concs[i] in filename) and (extension in filename) and (desired_file in filename):
 
-            chains=np.load(("/").join([electrode, folder, filename]))
-            plot_params(titles, chains[:, 25000:, :], positions=positions, label=concs[i])
-            #pints.plot.trace(chains)
-            #plt.show()
-#chains2=np.load('GC4_MCMC_1_low_ru')
-plt.show()
+
+    chains=np.load(("/").join([electrode, folder, filename]))
+    #plot_params(titles, chains[:, 25000:, :], positions=positions, label=concs[i])
+    trace_plots(titles, chains, graph_titles, rhat=True )
+    plt.show()
+
     #
     #plt.subplots_adjust(left=0.08, bottom=0.09, right=0.95, top=0.92, wspace=0.30, hspace=0.33)
 #plot_params(titles, chains2)

FTacv_experiments/harmonics_plotter.py
@@ -32,8 +32,8 @@ class harmonics:
             freq_idx=np.where((frequencies<(true_harm+(self.input_frequency*self.filter_val))) & (frequencies>true_harm-(self.input_frequency*self.filter_val)))
             filter_bit=top_hat[freq_idx]
             #test_hat[freq_idx]=1e5
-            #harmonics[i,np.where((frequencies<(true_harm+(self.input_frequency*self.filter_val))) & (frequencies>true_harm-(self.input_frequency*self.filter_val)))]=filter_bit
-            harmonics[i, 0:len(filter_bit)]=filter_bit
+            harmonics[i,np.where((frequencies<(true_harm+(self.input_frequency*self.filter_val))) & (frequencies>true_harm-(self.input_frequency*self.filter_val)))]=filter_bit
+            #harmonics[i, 0:len(filter_bit)]=filter_bit
             harmonics[i,:]=((np.fft.ifft(harmonics[i,:])))
         return harmonics
     def empty(self, arg):

FTacv_experiments/noramp_fitting_3.py
@@ -25,7 +25,7 @@ else:
 data_path="/experiment_data_2/"+exp_type
 Electrode="Yellow"
 folder="Noramp"
-for lcv_1 in range(1, 3):
+for lcv_1 in range(1, 11):
     Method =str(lcv_1)+"_cv"
     type="current"
     type2="voltage"
@@ -76,7 +76,7 @@ for lcv_1 in range(1, 3):
         'sampling_freq' : (1.0/200),
         'phase' : 3*(math.pi/2),
         "time_end": None,
-        'num_peaks': 50
+        'num_peaks': 30
     }
     solver_list=["Bisect", "Brent minimisation", "Newton-Raphson", "inverted"]
     likelihood_options=["timeseries", "fourier"]
@@ -133,8 +133,8 @@ for lcv_1 in range(1, 3):
     #noramp_fit.def_optim_list(["Ru","Cdl","CdlE1", "CdlE2",'omega',"phase","cap_phase"])
     #noramp_fit.dim_dict["gamma"]=0
     true_data=current_results
-    plt.plot(voltage_results, current_results, alpha=0.5)
-    plt.show()
+    #plt.plot(voltage_results, current_results)
+    #plt.show()
     fourier_arg=noramp_fit.kaiser_filter(current_results)
     if simulation_options["likelihood"]=="timeseries":
         cmaes_problem=pints.SingleOutputProblem(noramp_fit, time_results, true_data)
@@ -145,7 +145,7 @@ for lcv_1 in range(1, 3):
     CMAES_boundaries=pints.RectangularBoundaries(list([np.zeros(len(noramp_fit.optim_list))]), list([np.ones(len(noramp_fit.optim_list))]))
     noramp_fit.simulation_options["label"]="cmaes"
     #noramp_fit.simulation_options["test"]=True
-    num_runs=10
+    num_runs=20
     param_mat=np.zeros((num_runs,len(noramp_fit.optim_list)))
     score_vec=np.zeros(num_runs)
     for i in range(0, num_runs):
@@ -156,7 +156,7 @@ for lcv_1 in range(1, 3):
         if "E0_mean" in noramp_fit.optim_list and "k0_loc" in noramp_fit.optim_list:
             cmaes_fitting.set_parallel(False)
         else:
-            cmaes_fitting.set_parallel(True)
+            cmaes_fitting.set_parallel(False)
         found_parameters, found_value=cmaes_fitting.run()
         cmaes_results=noramp_fit.change_norm_group(found_parameters, "un_norm")
         print(list(cmaes_results))
@@ -185,7 +185,7 @@ for lcv_1 in range(1, 3):
         sim_options=resistance_type+"_"+"k0_disp"
     else:
         sim_options=resistance_type
-    filename=("_").join([folder,Method, sim_options])+".run1"
+    filename=("_").join([folder,Method, sim_options])+".run4"
     filepath=("/").join([dir_path, "Inferred_params", Electrode_save, run])
     noramp_fit.save_state(results_dict, filepath, filename, save_params)
     error=np.std(abs(np.subtract(cmaes_time, current_results)))
@@ -210,14 +210,14 @@ for lcv_1 in range(1, 3):
     for i in range(0, 10):
         mcmc = pints.MCMCController(log_posterior, 3, xs,method=pints.HaarioBardenetACMC)
         mcmc.set_parallel(True)
-        mcmc.set_max_iterations(30000)
+        mcmc.set_max_iterations(50000)
         chains=mcmc.run()
-        rhat_mean=np.mean(pints.rhat_all_params(chains))
+        rhat_mean=np.mean(pints.rhat_all_params(chains[:, 30000:, :]))
         #pints.plot.trace(chains)
         #plt.show()
-        if rhat_mean<1.2:
+        if rhat_mean<1.1:
             run="MCMC_runs/omega_nondim"
-            save_file=filename=("_").join([folder,Method, sim_options, "MCMC"])+".run1"
+            save_file=filename=("_").join([folder,Method, sim_options, "MCMC"])+".run4"
             filepath=("/").join([dir_path, "Inferred_params", Electrode_save, run])
             f=open(filepath+"/"+filename, "wb")
             np.save(f, chains)

FTacv_experiments/single_e_class_unified.py
@@ -85,10 +85,11 @@ class single_electron:
 
         else:
             if simulation_options["method"]=="sinusoidal":
-                self.nd_param.time_end=(self.nd_param.num_peaks)#/self.nd_param.omega)
+                self.nd_param.time_end=(self.nd_param.num_peaks/self.nd_param.omega)/self.nd_param.c_T0
             else:
                 self.nd_param.time_end=2*(self.nd_param.E_reverse-self.nd_param.E_start)
             self.times()
+            print(self.nd_param.time_end, self.time_vec[-1])
             if simulation_options["no_transient"]!=False:
                     transient_time=self.t_nondim(self.time_vec)
                     start_idx=np.where(transient_time>simulation_options["no_transient"])
@@ -489,14 +490,18 @@ class single_electron:
         if self.simulation_options["numerical_debugging"]!=False:
             self.numerical_plots(solver)
         else:
+            start=time.time()
             if self.simulation_options["dispersion"]==True:
                 #print("dispersion")
+
                 time_series=self.paralell_disperse(solver)
+
             else:
                 time_series=solver(self.nd_param_dict, self.time_vec, self.simulation_options["method"],-1, self.bounds_val)
         if self.simulation_options["no_transient"]!=False:
             time_series=time_series[self.time_idx:]
         time_series=np.array(time_series)
+        print(time.time()-start)
         #time_series=self.define_voltages()
         if self.simulation_options["likelihood"]=='fourier':
             filtered=self.kaiser_filter(time_series)